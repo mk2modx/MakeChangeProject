4837368baf65108f38c22688cb55c53e36a9e0b0
Mark Ag <mk2modx@icloud.com>
2019-07-12 20:58:30 -0600

    MakeChange unfinished revised 1

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -17,22 +17,46 @@ public class MakeChange {
 		tendered = kb.nextDouble();
 		
 		fullChange = tendered - totalCost;
+		fullChange = fullChange + 0.0005;
 		
 		System.out.printf("%.2f%n", fullChange);
 		
 		double change1 = fullChange * 100;
 		double dollar = (change1 - (change1 % 100));
+			
+			int twentyDollar = bills(dollar);
+			System.out.println(twentyDollar);
+			
 		System.out.println(dollar / 100 + " : Dollars");
 		
 		int coins = (int)(change1 - dollar);
-		System.out.println(coins);
 
-		coins = coins - (coins % 25);
-		int quarters = coins / 25;
+		int quarters = (coins - (coins % 25)) / 25 ;
 		System.out.println(quarters + " : Quarters");
 		
-		System.out.println(coins);
+		int coins2 =  (coins - (quarters * 25 ));
+		
+		int dimes = (coins2 - (coins2 % 10)) / 10 ;
+		System.out.println(dimes + " : Dimes");
+		
+		int coins3 = (coins2 - (dimes * 10));
+		int nickles = (coins3 - (coins % 5)) / 5 ;
+		
+		System.out.println(nickles + " : Nickles");
+		
+		System.out.println((coins3 - (nickles * 5)) + " : Pennies");
+		
 		
 	}
 
+	public static int bills( double dollar) {
+		int twenty = (int) dollar / 100;
+		twenty = (twenty - (twenty % 20)) / 20;
+			return twenty;	
+}
+	public static int tenbills( double dollar, int twentyDollar) {
+	int ten = 0;
+		
+		return ten;
+	}
 }
